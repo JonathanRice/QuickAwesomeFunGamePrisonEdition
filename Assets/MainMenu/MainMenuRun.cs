adbaaf7ba93069eba3ebaf800613f5c676109709
Jonathan Rice <jonathanrice42@gmail.com>
2014-05-31 15:09:30 -0400

    Added Main menu

---
+++ Assets/MainMenu/MainMenuRun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuRun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0)) {
+			loadNextLevel();
+		}
+	}
+	void loadNextLevel() {
+		//TODO some kind of transition
+		Application.LoadLevel("StealThePie");
+	}
+}
